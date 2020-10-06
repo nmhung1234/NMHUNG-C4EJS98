60c62bdf7a680f315deda1012e91fbb25de2c46d
nmhung1234 <nmhung1234@hotmail.com>
2020-10-06 20:11:26 +0700

    update ex7

--- SESSION2/BTVN/EXCERCISE/ex7.js
+++ SESSION2/BTVN/EXCERCISE/ex7.js
@@ -1,14 +1,17 @@
 // ex 7: unsatisfied
     let row7 = Number(prompt("Enter Rows7: "));
     let column7 = Number(prompt("Enter column7: "));
-    let array7 = new Array (column7);
+    //let array7 = new Array (column7);
+let string7 = "";
+let add = "*";
+for (let i = 0; i < row7; i++)
+{
+    for(let j = 0; j< column7; j++)
+    {
+        string7 += add; 
+    }
+    console.log(string7);
+    string7 = "";
+}
 
-    for(let i = 0; i < column7; i++)
-    {
-        array7[i] = '*';
-    }
-    for(let j = 0; j < row7; j++)
-    {
-       console.log(array7);
-    }
 
