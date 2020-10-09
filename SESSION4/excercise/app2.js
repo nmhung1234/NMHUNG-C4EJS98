6d6c3a0d866ca300b56a0594ac4e8fd26e75b20b
nmhung1234 <nmhung1234@hotmail.com>
2020-10-10 00:59:08 +0700

    update excercise

--- SESSION4/excercise/app2.js
+++ SESSION4/excercise/app2.js
@@ -1,4 +1,3 @@
-let action = prompt("Enter your action C,R,U,D,E");
 let phoneList = [
     {
         name: "Hung",
@@ -10,36 +9,41 @@ let phoneList = [
     }
 ];
 let contact = {};
-switch (action) {
-    case 'c':
-        {
-            add();
-            logout();
-            break;
-        }
-    case 'r':
-        {
-            logout();
-            break;
-        }
-
-    case 'u':
-        {
-            updateP();
-            logout();
-            break;
-        }
-
-    case 'd':
-        {
-            deleteP();
-            logout();
-            break;
-        }
-    case 'e':
-        break;
-}
-
+let action;
+let control = ['c', 'r', 'u', 'd', 'e'];
+do{
+    do{
+        action= String(prompt("Enter your action C,R,U,D,E").toLowerCase());
+    }while(control.indexOf(action) == -1)
+    
+    switch (action) {
+        case 'c':
+            {
+                add();
+                logout();
+                break;
+            }
+        case 'r':
+            {
+                logout();
+                break;
+            }
+    
+        case 'u':
+            {
+                updateP();
+                logout();
+                break;
+            }
+    
+        case 'd':
+            {
+                deleteP();
+                logout();
+                break;
+            }
+    }
+}while(action != 'e')
 
 function logout() {
     for (let people of phoneList)
@@ -47,7 +51,7 @@ function logout() {
 }
 function add() {
     let name = String(prompt("Enter the name"));
-    let phonenumber = String(prompt('Enter the phong number'));
+    let phonenumber = String(prompt('Enter the phone number'));
     contact['name'] = name;
     contact['phone'] = phonenumber;
     phoneList.push(contact);
@@ -55,20 +59,26 @@ function add() {
 function updateP() {
     let updateInf = prompt("Enter your name you want to update");
     let newName = String(prompt("Enter your new name you want to update"));
-    let newNumber = Number(prompt("Enter your Number name you want to update"));
+    let newNumber = Number(prompt("Enter your number name you want to update"));
     for (let i = 0; i < phoneList.length; i++) {
         if (phoneList[i].name == updateInf) {
             phoneList[i].name = newName;
             phoneList[i].phone = newNumber;
         }
+        else 
+            alert("Your name is not exist to update!!");
+        break;
     }
+
 }
 function deleteP() {
     let del = String(prompt("Enter your name you want to delete"));
     for (let i = 0; i < phoneList.length; i++) {
         if (phoneList[i].name == del) {
-            delete phoneList[i].name;
-            delete phoneList[i].phone;
+            phoneList.splice(i, 1);
         }
+        else 
+            alert("Your name is not exist to delete!!");
+        break;
     }
 }
